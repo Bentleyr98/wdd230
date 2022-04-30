ff9f8f0421aa437675b101ca1194be68847ccf02
Bentleyr98 <bentley.rebekah314@gmail.com>
2022-04-30 11:52:08 -0600

    final edits to lesson 2

--- scripts/main.js
+++ scripts/main.js
@@ -4,15 +4,6 @@ let year = date.getFullYear();
 document.querySelector('.date').textContent = currentdate;
 document.querySelector('.year').textContent = year;
 
-const lessons = ['Lesson 02','Lesson 03','Lesson 04', 'Lesson 05', 'Lesson 06', 'Lesson 07', 'Lesson 08', 'Lesson 09', 'Lesson 10','Lesson 11'];
-const titles = ['Design Principles', 'Title', 'Title', 'Title', 'Title', 'Title', 'Title','Title','Title','Title'];
-index=0;
-
-// while(index<lessons.length){
-//     document.querySelector('div').innerHTML += `<li> ${lessons[index]}: <a href="">${titles[index]}</a> </li>`;
-//     index++;
-// }
-
 document.querySelector('div').innerHTML += `<li> Lesson 02: <a href="lesson2/design-principles.html">Design Principles</a> </li>`;
 document.querySelector('div').innerHTML += `<li> Lesson 03: <a href="">Title</a> </li>`;
 document.querySelector('div').innerHTML += `<li> Lesson 04: <a href="">Title</a> </li>`;
