721f96e0253b11319e0e54f1eeed53ecbb96cc72
Bentleyr98 <bentley.rebekah314@gmail.com>
2022-05-16 11:10:50 -0600

    added interactive features

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,5 +1,18 @@
 let currentdate = document.lastModified;
 let date = new Date();
+
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+
 let year = date.getFullYear();
 document.querySelector('.date').textContent = currentdate;
 document.querySelector('.year').textContent = year;
+document.querySelector('.date_h').textContent = fulldateUK;
+
+let hambutton = document.querySelector('.ham');
+let mainnav = document.querySelector('nav');
+
+hambutton.addEventListener('click', ()=> {mainnav.classList.toggle('responsive')});
