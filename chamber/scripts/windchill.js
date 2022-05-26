31168f9fbd57a40e73346af6d6e8432e8c33efb6
Bentleyr98 <bentley.rebekah314@gmail.com>
2022-05-26 08:17:27 -0600

    changed background, added favicon and working on windchill

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,12 @@
+let temp = document.querySelector('temp');
+console.log(temp);
+let windSpeed = document.querySelector('wind');
+console.log(windSpeed);
+
+
+if(temp <= 50 & windSpeed > 3.0){
+    chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windSpeed,0.16)) + (0.4275*temp*Math.pow(windSpeed,0.16)));
+    document.querySelectorAll('chill').textContent = chill;
+} else{
+    document.querySelectorAll('chill').textContent = "N/A";
+}
