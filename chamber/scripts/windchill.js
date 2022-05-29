308575593351eb568b0567523e2351a4b51c6035
Bentleyr98 <bentley.rebekah314@gmail.com>
2022-05-28 23:46:10 -0600

    working on windchill

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,12 +1,12 @@
-let temp = document.querySelector('temp');
+let temp = parseInt(document.querySelector('temp').value);
 console.log(temp);
-let windSpeed = document.querySelector('wind');
+let windSpeed = document.querySelector('.wind').value;
 console.log(windSpeed);
 
 
-if(temp <= 50 & windSpeed > 3.0){
-    chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windSpeed,0.16)) + (0.4275*temp*Math.pow(windSpeed,0.16)));
-    document.querySelectorAll('chill').textContent = chill;
-} else{
-    document.querySelectorAll('chill').textContent = "N/A";
-}
+// if(temp <= 50 & windSpeed > 3.0){
+//     chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windSpeed,0.16)) + (0.4275*temp*Math.pow(windSpeed,0.16)));
+//     document.querySelectorAll('chill').textContent = chill;
+// } else{
+//     document.querySelectorAll('chill').textContent = "N/A";
+// }
