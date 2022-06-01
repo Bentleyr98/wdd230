96f820a5c972c66c969cfc515d3fe94ab284ab51
Bentleyr98 <bentley.rebekah314@gmail.com>
2022-06-01 13:30:32 -0600

    centered text, and fixing windchill

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,12 +1,13 @@
-let temp = parseInt(document.querySelector('temp'));
+let temp = Number(document.querySelector('.temp').textContent);
 console.log(temp);
-let windSpeed = document.querySelector('.wind').value;
+let windSpeed = Number(document.querySelector('.wind').textContent);
 console.log(windSpeed);
 
 
-// if(temp <= 50 & windSpeed > 3.0){
-//     chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windSpeed,0.16)) + (0.4275*temp*Math.pow(windSpeed,0.16)));
-//     document.querySelectorAll('chill').textContent = chill;
-// } else{
-//     document.querySelectorAll('chill').textContent = "N/A";
-// }
+if(temp <= 50 & windSpeed > 3.0){
+    chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windSpeed,0.16)) + (0.4275*temp*Math.pow(windSpeed,0.16)));
+    document.querySelectorAll('chill').textContent = chill;
+    console.log(chill);
+} else{
+    document.querySelectorAll('chill').textContent = chill;
+}
---
+++ scripts/image-load.js
@@ -0,0 +1,31 @@
+const images = document.querySelectorAll('img');
+const options = {threshold: .5, rootMargin: "0px 0px 100px 0px"};
+
+function preloadImage(img){
+    const src = img.getAttribute('data-src');
+    if(!src){
+        return;
+    } else{
+        img.src = src;
+    }
+}
+
+const io = new IntersectionObserver (
+    (entries, io) => {
+        entries.forEach(entry => {
+            console.log(entry);
+            if(!entry.isIntersecting){
+                return;
+            } else{
+                preloadImage(entry.target);
+                io.unobserve(entry.target);
+            }
+            })
+    }, 
+    options
+);
+
+images.forEach(img => {
+    io.observe(img);
+})
+
